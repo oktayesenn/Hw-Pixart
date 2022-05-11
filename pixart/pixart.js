dcb523b773c6efcb6d822a8dfb6a630874474f1d
Oktay esen <oktayesenn@gmail.com>
2022-05-10 22:28:18 -0400

    Answer1

--- pixart/pixart.js
+++ pixart/pixart.js
@@ -1,1 +1,7 @@
+let setColor = document.getElementById('set-color');
 
+setColor.addEventListener('click', function(event){
+    event.preventDefault();
+    let inputColor = document.getElementById('color-field').value;
+    document.querySelector('.brush').style.background = inputColor;
+})
