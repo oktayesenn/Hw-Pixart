98b19d711c8740b5b829e38deb00a2c53ffc8072
Oktay esen <oktayesenn@gmail.com>
2022-05-10 22:30:21 -0400

    Answer 2

--- pixart/pixart.js
+++ pixart/pixart.js
@@ -1,7 +1,16 @@
 let setColor = document.getElementById('set-color');
+let inputColor = document.getElementById('color-field');
 
 setColor.addEventListener('click', function(event){
     event.preventDefault();
     let inputColor = document.getElementById('color-field').value;
     document.querySelector('.brush').style.background = inputColor;
+})
+
+
+inputColor.addEventListener('keypress', function(event){
+    if (event.key === "Enter"){
+    document.querySelector('.brush').style.background = inputColor.value;
+    event.preventDefault();
+    }
 })
