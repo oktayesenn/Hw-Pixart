57b1fc297b9442dc2d72be18a396d09b60636453
Oktay esen <oktayesenn@gmail.com>
2022-05-17 22:12:08 -0400

    Oktay Upt

--- pixart/pixart.js
+++ pixart/pixart.js
@@ -1,30 +1,26 @@
 let setColor = document.getElementById('set-color');
 let inputColor = document.getElementById('color-field');
+let colorBox = document.querySelector('.brush');
+// console.log(setColor)
 
 setColor.addEventListener('click', function(event){
     event.preventDefault();
     let inputColor = document.getElementById('color-field').value;
     document.querySelector('.brush').style.background = inputColor;
 })
-
-
 inputColor.addEventListener('keypress', function(event){
     if (event.key === "Enter"){
     document.querySelector('.brush').style.background = inputColor.value;
     event.preventDefault();
     }
 })
-
-for(let i = 0; i < 5000; i++) {
+for(let i = 0; i < 20; i++) {
     let square = document.createElement('div');
     square.className = "square";
     square.id = i;
     document.body.appendChild(square);
-
 }
-
 let bodySquares = document.getElementsByClassName('square');
-
 for (let i = 0; i < bodySquares.length; i++) {
     bodySquares[i].addEventListener('mouseover', function (){
         document.getElementById(`${i}`).style.background = colorBox.style.background;
